dcb625d903c1dded928e16a7e6faddcc1710d0e2
Lunna Clarissa <lunnaclarissa98@gmail.com>
2026-05-15 08:53:31 -0300

    atualização

--- apps/clientes/forms.py
+++ apps/clientes/forms.py
@@ -7,7 +7,7 @@ class ClienteForm(forms.ModelForm):
     nome = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
     cpf =forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
     telefone = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
-    nascimento = forms.CharField(widget=forms.DateInput(attrs={"class":"form-control","type":"date"}))
+    datadenascimento = forms.CharField(widget=forms.DateInput(attrs={"class":"form-control","type":"date"}))
 
     class Meta:
         model = Cliente
